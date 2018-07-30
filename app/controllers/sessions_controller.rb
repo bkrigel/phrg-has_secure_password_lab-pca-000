52cae1fbb3e09a88e671904bed2be9163c599aed
bkrigel <benjamin.krigel@gmail.com>
2018-07-30 11:30:26 -0400

    oooohhhh BCrypt, how encrypted you are

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      redirect_to "/login"
+    end
+  end
+end
